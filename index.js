1bac53618378a0fb56929b5792eeb8fd05dd87ee
Ruchkina <ruch.nastya@gmail.com>
2023-09-17 11:09:37 +0300

    start

--- index.js
+++ index.js
@@ -11,63 +11,36 @@ app.use(express.json());
 
 // Настройка БД
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/vote-app');
+mongoose.connect('mongodb://127.0.0.1:27017/tick-tack-toe');
 
 // Схемы
-let votesSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    positive: Number,
-    negative: Number
+let goesSchema = new mongoose.Schema({
+    first:String,
+    second:String,
+    coordinats: String
 });
 
-let Vote = mongoose.model('votes', votesSchema);
+let Goes = mongoose.model('goes', goesSchema);
 
 // Раздача статики
 app.use(express.static(`public`));
 
 // Роуты API
 
-app.get(`/votes/all`, async function(req, res){
-    let votes = await Vote.find()
-    console.log(votes)
-    res.send(votes)
+app.get(`/`, async function(req, res){
+    
 })
 
-app.post(`/votes/create`, async function(req, res){
-    let title1 = req.body.title;
-    let description1 = req.body.description;
-    let vote = new Vote({
-        title: title1,
-        description: description1,
-        positive: 0,
-        negative: 0
-    })
+// app.post(`/votes/create`, async function(req, res){
+//     let title1 = req.body.title;
+//     let description1 = req.body.description;
+//     let vote = new Vote({
+//         title: title1,
+//         description: description1,
+//         positive: 0,
+//         negative: 0
+//     })
 
-    await vote.save();
-    res.send(vote)
-})
-
-app.post(`/votes/remove`, async function(req, res){
-    let id = req.body.id;
-    await Vote.deleteOne({_id:id});
-    res.send(200)
-})
-
-app.post(`/votes/positive`, async function(req, res){
-    let id = req.body.id;
-    let vote = await Vote.findOne({_id:id});
-    vote.positive++;
-    await vote.save();
-
-    res.send(vote)
-})
-
-app.post(`/votes/negative`, async function(req, res){
-    let id = req.body.id;
-    let vote = await Vote.findOne({_id:id});
-    vote.negative++;
-    await vote.save();
-
-    res.send(vote)
-})
+//     await vote.save();
+//     res.send(vote)
+// })
